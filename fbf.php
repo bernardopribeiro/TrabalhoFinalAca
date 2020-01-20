7ca5ba59fb7c7ba18a459abaf91c26c95a53c149
Bernardo <bernardopribeiro@hotmail.com>
2020-01-20 16:49:42 +0000

    Alteração no fbf.php, para não fechar o programa enquanto não inserir um valor válido.

--- fbf.php
+++ fbf.php
@@ -7,42 +7,53 @@ echo "\n-> Opção 1 - Procurar ficheiros por extensão e diretoria.";
 echo "\n-> Opção 2 - Procura personalizada.";
 echo "\n-> Opção 0 - Sair";
 
-echo "\nEnter a number: ";
+echo "\nQual a opão: ";
 fscanf(STDIN, "%d\n", $number);
-
+while ($number != 1 && $number != 2 && $number != 0) {
+    echo "\nQual a opão: ";
+    fscanf(STDIN, "%d\n", $number);
+}
 switch ($number) {
     case "1":
+        echo "\n-> Opção 1 - Procurar ficheiros por extensão e diretoria.";
         escolherOpcaoUm();
         break;
     case "2":
-        echo "\n***** Sub Menu *****";
+        echo "\n***** PROCURA PERSONALIZADA *****";
         echo "\n-> Opção 1 - Procurar ficheiro mais recente.";
         echo "\n-> Opção 2 - Procurar ficheiro maior.";
         echo "\n-> Opção 3 - Procurar ficheiro mais pequeno.";
         echo "\n-> Opção 4 - Procurar ficheiro mais antigo.";
         echo "\n-> Opção 0 - Sair";
 
-        echo "\nEnter a number: ";
+        echo "\nQual a opão: ";
+
         fscanf(STDIN, "%d\n", $number);
 
+        while ($number != 1 && $number != 2  && $number != 3 && $number != 4 && $number != 0) {
+            echo "\nQual a opão: ";
+            fscanf(STDIN, "%d\n", $number);
+        }
         switch ($number) {
             case "1":
+                echo "\n-> Opção 1 - Procurar ficheiro mais recente.";
                 escolherOpcao(1);
                 break;
             case "2":
+                echo "\n-> Opção 2 - Procurar ficheiro maior.";
                 escolherOpcao(2);
                 break;
             case "3":
+                echo "\n-> Opção 3 - Procurar ficheiro mais pequeno.";
                 escolherOpcao(3);
                 break;
             case "4":
+                echo "\n-> Opção 4 - Procurar ficheiro mais antigo.";
                 escolherOpcao(4);
                 break;
             case "0":
                 exit();
                 break;
-            default:
-                echo "Apenas pode escolher 1, 2,3,4 e 0.";
         }
         break;
     case "0":
